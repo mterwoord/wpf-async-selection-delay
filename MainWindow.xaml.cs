e8d602d45de37b5800c33259a602f9a9a9bd5465
Matthijs ter Woord <matthijs@terwoord.eu>
2015-03-29 11:54:56 +0200

    Initial commit.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAsyncSelectionDelay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public static readonly DependencyProperty InputProperty =
+            DependencyProperty.Register("Input", typeof(string), typeof(MainWindow),
+                new FrameworkPropertyMetadata(null, OnInputChanged));
+
+        public string Input
+        {
+            get { return (string)GetValue(InputProperty); }
+            set { SetValue(InputProperty, value); }
+        }
+
+        private static void OnInputChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var xTarget = (MainWindow)d;
+            var xOldInput = (string)e.OldValue;
+            var xNewInput = xTarget.Input;
+            xTarget.OnInputChanged(xOldInput, xNewInput);
+        }
+
+        protected virtual async void OnInputChanged(string oldInput, string newInput)
+        {
+            if (mCancelToken != null)
+            {
+                mCancelToken.Cancel();
+                mCancelToken.Dispose();
+                mCancelToken = null;
+            }
+
+            mCancelToken = new CancellationTokenSource();
+
+            try
+            {
+                await Task.Delay(1000, mCancelToken.Token);
+            }
+            catch (TaskCanceledException)
+            {
+                return;
+            }
+            HandleValueChanged();
+        }
+
+        private CancellationTokenSource mCancelToken;
+
+        private void HandleValueChanged()
+        {
+            AppendLog(string.Format("New text: '{0}'", Input));
+        }
+
+        private void AppendLog(string line)
+        {
+            var xLog = blockLog.Text;
+            xLog = xLog + "\r\n" + line;
+            blockLog.Text = xLog.Trim();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+﻿This sample project shows how to implement a delay in a property change, using async task cancellation.
+The same can be achieved by the WPF Binding.Delay property, but I found that in some situations this doesn't work as expected.
+
